5276f43040f7224cff2db0438b9ca947efde5fdc
Rodrigo M. Mariano <rodmariano13@gmail.com>
2017-07-25 17:28:06 -0300

    Create list of checkbox layers and radio button layers

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,63 @@
 
 $(document).ready(function(){
 
-    // when change the radio buttom, change the base map
     $('#layers input[type=radio]').change(function() {
-        var layer = $(this).val();
+        // This action get the event of the radio button of base map
+        // when select it, showing it and hiding the others
 
-        map.getLayers().getArray().forEach(function(e) {
-            var name = e.get('name');
-            e.setVisible(name == layer);
-        });
+        // var layer = $(this).val();
+
+        // map.getLayers().getArray().forEach(function(e) {
+        //     var name = e.get('name');
+        //
+        //     if (e.get("always_visible_it")) {}
+        //
+        //     e.setVisible(name == layer);
+        // });
+
+
+        var layers_name = $(this).val();
+
+        // var __layers__ = map.getLayers().getArray();
+        var __layers__ = layers_radio_button;
+
+        for (var i=0; i<__layers__.length; i++) {
+            var layer = __layers__[i];
+
+            var name = layer.get('name');
+
+            // if the layer is to be always visible, so doesn't mess in it
+            // if (layer.get("always_visible_it"))
+            //     continue;
+
+            layer.setVisible(name == layers_name);
+
+        }
+
+
         // independently of base map, the layer that do creation/modification
         // of geometry stay visible
-        layer_feature_overlay.setVisible(true);
-        layer_geoserver_wms_sara_brasil.setVisible(true);
+        // layer_feature_overlay.setVisible(true);
+        // layer_geoserver_wms_pauliceia_sara_brasil.setVisible(true);
     });
 
 
     $("#div_checkbox_layers input[type='checkbox']").change(function() {
-        if(this.checked) {
-            console.log("checked: " + this.id);
-        }else{
-            console.log("unchecked: " + this.id);
+        // This action get the event of the checkbox when select or unselect it,
+        // so show or hide the layer equivalent
+
+        // this.checked
+        var layers = layers_checkbox;
+
+        for (var i=0; i<layers.length; i++) {
+            var layer = layers[i];
+
+            if (layer.getProperties()["name"] == this.id) {
+                // invert the visible: if true will be false, and vice-versa
+                layer.setVisible(!layer.getVisible());
+                break;
+            }
         }
-    });
-
-    $('input:checkbox[name=chkbx_pauliceia]:checked').change(function() {
-
-        var check = $(this).val();
-        console.log(check);
 
     });
 
